fbb0c1c53e08fadcfd1af3b7cf650b5fce3751d2
schmoller <steven.schmoll@gmail.com>
2016-09-08 12:57:30 +1000

    Added main class and configured gradle to set manifest

---
+++ src/main/java/sticklings/SticklingsMain.java
@@ -0,0 +1,15 @@
+package sticklings;
+
+/**
+ * Main class for Sticklings
+ */
+public class SticklingsMain {
+	/**
+	 * Entry point
+	 * @param args CL args, not used
+	 */
+	public static void main(String[] args) {
+		// For testing gradle jar
+		System.out.println("DEBUG: Test");
+	}
+}

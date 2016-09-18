b937860ab404d6f40627c4094244260865213aa3
Schmoller <steven.schmoll@gmail.com>
2016-09-18 21:54:49 +1000

    Implemented scene system and base entity

--- src/main/java/sticklings/scene/Entity.java
+++ src/main/java/sticklings/scene/Entity.java
@@ -1,25 +1,129 @@
 package sticklings.scene;
 
+import com.google.common.base.Preconditions;
+
+import sticklings.render.AbstractTexture;
+import sticklings.render.NullTexture;
+
+/**
+ * Represents an entity in a scene
+ */
 public abstract class Entity {
-	public Entity(Scene scene) {
+	private int posX;
+	private int posY;
+	private AbstractTexture texture;
+	
+	private int entityId;
+	private Scene scene;
+	
+	/**
+	 * Creates a new entity.
+	 */
+	public Entity() {
+		this.texture = NullTexture.get();
+	}
+	
+	/**
+	 * Sets the scene this entity belongs to
+	 * @param scene The scene object
+	 * @param entityId The id of the entity within the scene
+	 */
+	void setScene(Scene scene, int entityId) {
+		Preconditions.checkNotNull(scene);
 		
+		this.entityId = entityId;
+		this.scene = scene;
 	}
 	
+	/**
+	 * Gets the ID of this entity.
+	 * All entities have an ID, it may or may not be added to the scene
+	 * @return The ID
+	 */
+	public int getEntityId() {
+		return entityId;
+	}
+	
+	/**
+	 * Gets the X coordinate of the entity
+	 * @return The X coord
+	 */
 	public final int getX() {
-		throw new UnsupportedOperationException("Not yet implemented");
+		return posX;
 	}
 	
+	/**
+	 * Gets the Y coordinate of the entity
+	 * @return The Y coord
+	 */
 	public final int getY() {
-		throw new UnsupportedOperationException("Not yet implemented");
+		return posY;
 	}
 	
+	/**
+	 * Sets the X coord of the entity
+	 * @param x The new X coord
+	 */
+	public void setX(int x) {
+		posX = x;
+	}
+	
+	/**
+	 * Sets the Y coord of the entity
+	 * @param y The new Y coord
+	 */
+	public void setY(int y) {
+		posY = y;
+	}
+	
+	/**
+	 * Sets the X and Y position of the entity
+	 * @param x The new X coord
+	 * @param y The new Y coord
+	 */
+	public void setLocation(int x, int y) {
+		posX = x;
+		posY = y;
+	}
+	
+	/**
+	 * Gets the owning scene for this entity
+	 * @return The scene
+	 */
 	public final Scene getScene() {
-		throw new UnsupportedOperationException("Not yet implemented");
+		return scene;
 	}
 	
+	/**
+	 * Gets the current texture for this entity
+	 * @return The texture
+	 */
+	public AbstractTexture getTexture() {
+		return texture;
+	}
+	
+	/**
+	 * Sets the current texture for this entity.
+	 * @param texture The new texture, null will use NullTexture
+	 */
+	public void setTexture(AbstractTexture texture) {
+		if (texture == null) {
+			this.texture = NullTexture.get();
+		} else {
+			this.texture = texture;
+		}
+	}
+	
+	/**
+	 * Marks the entity for removal in the scene.
+	 */
 	public final void remove() {
 		throw new UnsupportedOperationException("Not yet implemented");
 	}
 	
-	public abstract void update();
+	/**
+	 * Updates the entity
+	 * @param deltaTime The time in seconds between the last update and this one
+	 */
+	public abstract void update(double deltaTime);
 }
--- src/main/java/sticklings/scene/Scene.java
+++ src/main/java/sticklings/scene/Scene.java
@@ -1,23 +1,129 @@
 package sticklings.scene;
 
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Map;
+import java.util.Set;
+
+import com.google.common.base.Preconditions;
+import com.google.common.collect.Iterables;
+import com.google.common.collect.Maps;
+import com.google.common.collect.Sets;
+
+/**
+ * Represents an entire scene, including the entities and terrain
+ */
 public class Scene {
+	private final Map<Integer, Entity> entityMap;
+	
+	// In order to prevent CME's, changes to the entity map must be postponed
+	// until updating is finished
+	private boolean isUpdating;
+	private final Map<Integer, Entity> toAdd;
+	private final Set<Integer> toRemove;
+	
+	private int nextEntityId;
+	
+	/**
+	 * Constructs a new empty scene
+	 */
 	public Scene() {
+		entityMap = Maps.newHashMap();
 		
+		isUpdating = false;
+		toAdd = Maps.newHashMap();
+		toRemove = Sets.newHashSet();
+		
+		nextEntityId = 0;
 	}
 	
-	public void update() {
-		throw new UnsupportedOperationException("Not yet implemented");
+	/**
+	 * Adds an entity to the scene.
+	 * If an update is in progress, the entity will not be added until the update is over
+	 * @param entity The entity to add
+	 */
+	public void addEntity(Entity entity) {
+		Preconditions.checkNotNull(entity);
+		
+		int id = nextEntityId++;
+		entity.setScene(this, id);
+		
+		if (isUpdating) {
+			toAdd.put(id, entity);
+		} else {
+			entityMap.put(id, entity);
+		}
 	}
 	
+	/**
+	 * Marks an entity for removal.
+	 * If an update is in progress, the entity will be removed after the update is over
+	 * @param entityId The ID of the entity to remove
+	 */
+	public void removeEntity(int entityId) {
+		if (isUpdating) {
+			toRemove.add(entityId);
+		} else {
+			entityMap.remove(entityId);
+		}
+	}
+	
+	/**
+	 * Gets an entity by ID
+	 * @param id the ID of the entity
+	 * @return The Entity or null
+	 */
 	public Entity getEntity(int id) {
-		throw new UnsupportedOperationException("Not yet implemented");
+		if (isUpdating) {
+			// Consider that the entity may not yet be added
+			Entity ent = entityMap.get(id);
+			if (ent != null) {
+				return ent;
+			} else {
+				return toAdd.get(id);
+			}
+		} else {
+			return entityMap.get(id);
+		}
 	}
 	
+	/**
+	 * Updates all entities within the scene
+	 * @param deltaTime The time in seconds between the last update and this
+	 */
+	public void update(double deltaTime) {
+		isUpdating = true;
+		for (Entity entity : entityMap.values()) {
+			entity.update(deltaTime);
+		}
+		isUpdating = false;
+		
+		// Add all new
+		entityMap.putAll(toAdd);
+		toAdd.clear();
+		
+		// Remove all marked
+		for (Integer id : toRemove) {
+			entityMap.remove(id);
+		}
+		toRemove.clear();
+	}
+	
+	/**
+	 * Gets all entities of the given type
+	 * @param type The type of entity to get
+	 * @return An iterable that contains the entities
+	 */
 	public <T extends Entity> Iterable<T> findEntities(Class<T> type) {
-		throw new UnsupportedOperationException("Not yet implemented");
+		return Iterables.filter(getAllEntities(), type);
 	}
 	
-	public Iterable<Entity> getAllEntities() {
-		throw new UnsupportedOperationException("Not yet implemented");
+	/**
+	 * Gets all entities that are within this scene.
+	 * NOTE: Care should be taken that no entities are added or removed while iterating on this collection
+	 * @return An unmodifiable collection of entities
+	 */
+	public Collection<Entity> getAllEntities() {
+		return Collections.unmodifiableCollection(entityMap.values());
 	}
 }

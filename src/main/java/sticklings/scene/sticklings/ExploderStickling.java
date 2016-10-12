60fa5075d4115f476cee7cf45fa3b310f5334e45
D-Swann <denholm_swann@me.com>
2016-10-12 19:45:52 +1100

    final exploder set up
    changed the explode size to 50 and removed the death label

--- src/main/java/sticklings/scene/sticklings/ExploderStickling.java
+++ src/main/java/sticklings/scene/sticklings/ExploderStickling.java
@@ -1,8 +1,5 @@
 package sticklings.scene.sticklings;
 
-import javafx.geometry.Pos;
-import javafx.scene.control.Label;
-import javafx.scene.paint.Paint;
 import sticklings.Game;
 import sticklings.scene.MovementController.MovementType;
 import sticklings.util.Location;
@@ -17,13 +14,8 @@ public class ExploderStickling extends Stickling {
     private State state;
     private int stepCount;
     private int nextStateChange;
-    
-    private Label death_counter = new Label();
-    
+
     public ExploderStickling(){
-        death_counter.setAlignment(Pos.CENTER);        
-        death_counter.setTextFill(Paint.valueOf("White"));
-        
         setTexture(Game.getInstance().getTextureManager().getTexture("/sprites/stickling/exploder.png"));
         setTextureOffset(new Location(-10, -10));
         setState(State.Walking);
@@ -34,7 +26,7 @@ public class ExploderStickling extends Stickling {
         stepCount = 0;
         if (state == State.Exploding){
             Location pos = getLocation();
-            getScene().getTerrain().clearCircleAt((int)pos.x, (int)pos.y, 30);
+            getScene().getTerrain().clearCircleAt((int)pos.x, (int)pos.y, 50);
             remove();
         }else {
             locomotor.allowMovement(MovementType.Walk);
@@ -44,10 +36,7 @@ public class ExploderStickling extends Stickling {
 
     @Override
     protected void operate() {
-        death_counter.setLayoutX(getLocation().x);
-        death_counter.setLayoutY(getLocation().y + 20);
-        death_counter.setText(String.format("%d", DEATH_TIME));
-        System.out.print(String.format("%d", DEATH_TIME));
+        //System.out.print(String.format("%d", DEATH_TIME));
         if (stepCount > nextStateChange){
             DEATH_TIME = DEATH_TIME -1;
             if (DEATH_TIME == 0){
